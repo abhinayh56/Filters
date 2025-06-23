b8c46da677c1a9d262d54521e62d3565285fc2a4
abhinayh56 <kumarabhinay53@gmail.com>
2025-06-23 12:03:37 +0530

    asdfsdf

--- main.cpp
+++ main.cpp
@@ -5,5 +5,16 @@ int main()
 {
     std::cout << "sadfsadf-----\n";
 
+    LPF_1<double> obj_LPF_1;
+    LPF_2<double> obj_LPF_2;
+    HPF_1<double> obj_HPF_1;
+    HPF_2<double> obj_HPF_2;
+    BPF_2<double> obj_BPF_2;
+    BSF_2<double> obj_BSF_2;
+    MA<double>    obj_MA;
+    SRL<double>   obj_SRL;
+
+    std::cout << "jjjjjjjjjjjjjjjjj\n";
+
     return 0;
 }
